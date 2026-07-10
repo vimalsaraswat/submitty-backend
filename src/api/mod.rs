447a48deb7484bbedda3b28f2a9b7c4d41cb074a
Vimal Saraswat <saraswatvimal1@gmail.com>
2026-07-10 22:32:17 +0530

    feat: add auth module and 404 fallback handler

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,14 +1,23 @@
+pub mod auth;
 pub mod user;
+
+use crate::AppState;
 use crate::api::user::user_router;
 
-use axum::{Router, response::IntoResponse};
+// use axum::middleware;
+use axum::{Router, http::StatusCode, response::IntoResponse};
+
+pub fn api_router() -> Router<AppState> {
+    Router::new()
+        .route("/health", axum::routing::get(health_handler))
+        .nest("/user", user_router())
+        .fallback(handler_404)
+}
 
 async fn health_handler() -> impl IntoResponse {
     "Service is operational and healthy."
 }
 
-pub fn api_router() -> Router {
-    Router::new()
-        .route("/health", axum::routing::get(health_handler))
-        .nest("/user", user_router())
+async fn handler_404() -> impl IntoResponse {
+    (StatusCode::NOT_FOUND, "nothing to see here")
 }
