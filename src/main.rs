2d5f5928204b83cd580da8ae94a7594a6e109ff8
Vimal Saraswat <saraswatvimal1@gmail.com>
2026-07-10 22:34:26 +0530

    feat: integrate database and app state

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,54 @@
 mod api;
+mod config;
+mod entities;
+mod error;
 mod oauth;
-use crate::api::api_router;
+use crate::{api::api_router, config::Config};
 
 use axum::Router;
-use std::env;
+use sea_orm::{Database, DatabaseConnection};
 use tower_http::trace::{DefaultMakeSpan, DefaultOnResponse, TraceLayer};
 use tracing::Level;
 use tracing_subscriber::{EnvFilter, fmt};
 
+#[derive(Clone, Debug)]
+struct AppState {
+    pub env: Config,
+    pub conn: DatabaseConnection,
+}
+
 #[tokio::main]
 async fn main() {
     dotenvy::dotenv().ok();
+    let config = Config::init();
 
     let env_filter = EnvFilter::try_from_default_env()
         .unwrap_or_else(|_| EnvFilter::new("info"))
         .add_directive("tower_http=debug".parse().unwrap());
     fmt().with_env_filter(env_filter).with_target(false).init();
 
-    let host = env::var("HOST").unwrap_or_else(|_| "0.0.0.0".into());
-    let port = env::var("PORT").unwrap_or_else(|_| "8080".into());
-    let server_url = format!("{host}:{port}");
+    let conn = Database::connect(&config.db_url)
+        .await
+        .expect("Database connection failed");
 
-    let listener = tokio::net::TcpListener::bind(&server_url).await.unwrap();
-    tracing::info!("Server starting at {}", server_url);
+    let state = AppState {
+        env: config.clone(),
+        conn,
+    };
 
-    let router = Router::new().nest("/api/v1", api_router()).layer(
-        TraceLayer::new_for_http()
-            .make_span_with(DefaultMakeSpan::new().level(Level::INFO))
-            .on_response(DefaultOnResponse::new().level(Level::INFO)),
-    );
+    let listener = tokio::net::TcpListener::bind(&config.server_url)
+        .await
+        .unwrap();
+    tracing::info!("Server starting at {}", &config.server_url);
+
+    let router = Router::new()
+        .nest("/api/v1", api_router())
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(DefaultMakeSpan::new().level(Level::INFO))
+                .on_response(DefaultOnResponse::new().level(Level::INFO)),
+        )
+        .with_state(state);
 
     axum::serve(listener, router).await.unwrap();
 }
